dd9f8215c3f56ec56a803a090281806cc8eae30a
RiturajAgrahari <agr.rituraj@gmail.com>
2024-04-07 12:21:48 +0530

    uploading the previous react app through vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import SignUp from './components/authentication/signup'
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp></SignUp>
+    </div>
+  )
+}
+
+export default App
